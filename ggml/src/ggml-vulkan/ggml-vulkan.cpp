8eb56cd6103c2c1adb22fcd34390417d659a827c
Nakasaka, Masato <masato.nakasaka@intel.com>
2026-02-04 00:32:02 -0800

    experimenting

--- ggml/src/ggml-vulkan/ggml-vulkan.cpp
+++ ggml/src/ggml-vulkan/ggml-vulkan.cpp
@@ -5934,7 +5934,7 @@ static void ggml_vk_dispatch_pipeline(ggml_backend_vk_context* ctx, vk_context& 
     const uint32_t wg0 = CEIL_DIV(elements[0], pipeline->wg_denoms[0]);
     const uint32_t wg1 = CEIL_DIV(elements[1], pipeline->wg_denoms[1]);
     const uint32_t wg2 = CEIL_DIV(elements[2], pipeline->wg_denoms[2]);
-    GGML_LOG_INFO("ggml_vk_dispatch_pipeline(%s)\n", pipeline->name.c_str());
+    //GGML_LOG_INFO("ggml_vk_dispatch_pipeline(%s)\n", pipeline->name.c_str());
     VK_LOG_DEBUG("ggml_vk_dispatch_pipeline(" << pipeline->name << ", {";
     for (auto& buffer : descriptor_buffer_infos) {
         std::cerr << "(" << buffer.buffer << ", " << buffer.offset << ", " << buffer.range << "), ";
@@ -12365,7 +12365,7 @@ static void ggml_vk_compute_forward(ggml_backend_vk_context * ctx, ggml_cgraph *
             memset(mset.dst, mset.val, mset.n);
         }
         
-        auto start = std::chrono::high_resolution_clock::now();
+        //auto start = std::chrono::high_resolution_clock::now();
         if (almost_ready && !ctx->almost_ready_fence_pending) {
             ggml_vk_submit(subctx, ctx->almost_ready_fence);
             ctx->almost_ready_fence_pending = true;
@@ -12373,15 +12373,15 @@ static void ggml_vk_compute_forward(ggml_backend_vk_context * ctx, ggml_cgraph *
             ggml_vk_submit(subctx, {});
         }
         ctx->submit_pending = true;
-        ggml_vk_synchronize(ctx);
-        auto end = std::chrono::high_resolution_clock::now();
+        //ggml_vk_synchronize(ctx);
+        //auto end = std::chrono::high_resolution_clock::now();
 
 #ifdef GGML_VULKAN_CHECK_RESULTS
         ggml_vk_synchronize(ctx);
         ggml_vk_check_results_1(ctx, cgraph, tensor_idx);
 #endif
-        auto usec = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
-        GGML_LOG_INFO("ggml_vk_compute_forward: %s %zu us\n", ggml_op_name(tensor->op), usec);
+        //auto usec = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
+        //GGML_LOG_INFO("ggml_vk_compute_forward: %s %zu us\n", ggml_op_name(tensor->op), usec);
     }
 
     if (tensor_idx == subctx->exit_tensor_idx) {
@@ -13353,7 +13353,7 @@ static ggml_status ggml_backend_vk_graph_compute(ggml_backend_t backend, ggml_cg
     // Estimate the amount of matmul work by looking at the weight matrix size, and submit every 100MB
     // (and scaled down based on model size, so smaller models submit earlier).
     // Also submit at least every 100 nodes, in case there are workloads without as much matmul.
-    int nodes_per_submit = 1;
+    int nodes_per_submit = 100;
     int submitted_nodes = 0;
     int submit_count = 0;
     uint64_t mul_mat_bytes = 0;
