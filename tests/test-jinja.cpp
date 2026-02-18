003b4f8c25a67fd208272b89748fd0573122104d
Nakasaka, Masato <masato.nakasaka@intel.com>
2026-02-17 22:43:57 -0800

    force LF on Windows for failing jinja test

--- tests/test-jinja.cpp
+++ tests/test-jinja.cpp
@@ -1796,6 +1796,9 @@ import sys
 from datetime import datetime
 from jinja2.sandbox import SandboxedEnvironment
 
+# Force LF print even on Windows so test results match expected
+sys.stdout.reconfigure(newline="\n")
+
 tmpl = json.loads(sys.argv[1])
 vars_json = json.loads(sys.argv[2])
 
